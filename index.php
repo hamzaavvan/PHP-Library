1b11a49aa964650be44161df03e9eaa15170efef
Hamza AVvan <hamzaavvan@gmail.com>
2016-09-24 20:56:18 +0500

    Update index.php

--- index.php
+++ index.php
@@ -2,19 +2,23 @@
 
 require_once 'Validate.php';
 
+// Instantiating the Validate object
 $validate = new Validate();
 
+// checking for user fields
 if (Input::exists()) {
 	$validate->valid([
-		'age' => 'required|length<=:4',
+		'age' => 'required|length>=:4',
 	]);
 }
 
 ?>
 
+<!-- Simple test form -->
 <form method="post">
 	Name: <input type="text" name="age" />
 	<input type="submit" value="Check">
 </form>
+
 
 <?php echo $validate->error(); ?>
