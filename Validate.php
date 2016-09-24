0137471805989dc259ca4fe8ef1dd28b8c834d57
Hamza AVvan <hamzaavvan@gmail.com>
2016-09-24 20:20:58 +0500

    Update Validate.php

--- Validate.php
+++ Validate.php
@@ -1,19 +1,47 @@
 <?php
 
-class Validate extends CRUD {
-	public  $session_error = false,
-			$message = '',
-			$session_type = 'session',
-			$address;
+require_once 'Validationbag.php';
+
+class Validate {
+	/** To use the methods of ValidationBag trait */
+	use ValidationBag;
+
+	/**
+	 * to examine the flow errors, this property will be set to true if there will be any error caught by @ValidationBag trait
+	 *
+	 * @var bool $session_error
+	 */
+	public  $session_error = false;
+
+	/**
+	 * this property will be populated with simple string when any error catched caught by @ValidationBag trait
+	 *
+	 * @var string $message
+	 */
+	public $message = '';
+
+	/**
+	 * this property is totally dependant on checksBag method provided by @ValidationBag
+	 *
+	 * @param string $session_type
+	 */
+	public $session_type = 'session';
+
+	/**
+	 * 
+	 */
+	public $address;
 
 	/**
 	 * Data stored when a form submits 
+	 *
 	 * @var array $userData
 	 */
 	private $userData;
 
 	/**
 	 * Info to be ignore 
+	 *
 	 * @var array $ignorable
 	 */
 	private $ignorable = ["token"];
@@ -30,6 +58,8 @@ class Validate extends CRUD {
 		// Reversed that array for delivering proper functionality in nested loops
 		$array = array_reverse($array);
 
+		self::$fieldsCount = count($array);
+
 		foreach ($array as $key => $value) {
 			$keys = $this->split('|', $value, true);
 
@@ -37,6 +67,7 @@ class Validate extends CRUD {
 				switch ($value) {
 					case 'required': // check wether required fields are empty
 						$this->required($key);
+
 					break;
 
 					case 'sanitize': // will sanitize specified field
@@ -49,14 +80,14 @@ class Validate extends CRUD {
 						$length = $keys[1];
 						$operator = $this->split('length', $keys[0])[1];
 
-						$this->check_length($key, $operator, $length);
+						$this->checkLength($key, $operator, $length);
 					break;
 
 					case (preg_match('/^checked/', $value) == 1) ? $value : '' :
 						$keys = $this->split(':', $value);						
 						$action = $keys[1];
 
-						$this->checkbox($action, $key);
+						$this->checksBag($action, $key);
 					break;
 
 					case 'exists':
@@ -65,33 +96,13 @@ class Validate extends CRUD {
 
 					case 'csrf':
 						if (!Token::check(Input::get($key))) {
-							$this->setError();
+							$this->throwError();
 						}
 					break;
 				}				
 			}
 		}
 		return $this;
-	}
-
-	/**
-	 * to set default error
-	 *
-	 * @param string | array $message
-	 * @return void
-	 */
-	public function setError($message = '') {
-			$this->session_error = true;
-			$this->message = $message;
-	}
-
-	/**
-	 * to get the value set by setError() method
-	 *
-	 * @return bool $session_error
-	 */
-	public function valErr() {
-		return $this->session_error;
 	}
 
 	public function userData() {
@@ -104,6 +115,7 @@ class Validate extends CRUD {
 
 				if (!in_array($keys, $this->ignorable)) {
 					$this->userData[$keys] = ($keys == "password") ? crc32($values) : $values;
+				}
 			});
 
 			return $this->userData;
@@ -125,138 +137,5 @@ class Validate extends CRUD {
 			$keys = explode($delimeter, $value); // Simply explode the string
 		}		
 		return $keys;
-	}
-
-	/**
-	 * set the supplied field as mandatory
-	 *
-	 * @param $var
-	 * @return void
-	 */
-	private function required($var) {
-		if (empty(Input::get($var))) {
-			$this->setError("All fields are required!");
-		}
-	}
-
-	/**
-	 * sanitize the user supplied data
-	 *
-	 * @param string $key
-	 * @return void
-	 */
-	private function sanitize($key) {
-		switch ($key) {
-			case 'username':
-				if (preg_match('/ /', Input::get($key)) == true) {
-					$this->setError("Usename must not contain any spaces!");
-				}else if (filter_var(Input::get($key), FILTER_VALIDATE_EMAIL) == true) {
-					$this->setError("Usename must not be like email!");
-				}
-			break;
-
-			case 'email':
-				if (filter_var(Input::get($key), FILTER_VALIDATE_EMAIL) == false)
-				{
-					$this->setError("Incorrect email address!");
-				}				
-			break;
-		}
-	}
-
-	/**
-	 * for checking the length of passed $variable along with an $operator and limited $length
-	 * The argument for the following case should be in this way:
-	 * length[operators]:[length]
-	 *
-	 * @param string $var
-	 * @param array $operators = ['==', '!=', '>', '<', '>=', '<=']
-	 * @param bool $length
-	 */
-	private function check_length($var, $operator, $length) {
-		$name = ucfirst($var);
-		switch ($operator) {			
-			case '==':
-				if (strlen(Input::get($var)) != $length) {
-					$this->setError($name.' must be equal to '.$length.' characters');
-				}
-			break;
-			case '!=':
-				if (strlen(Input::get($var)) == $length) {
-					$this->setError($name.' must not be equal to '.$length.' characters');
-				}
-			break;
-			case '<':
-				if (strlen(Input::get($var)) > $length) {
-					$this->setError($name.' must be less than '.$length.' characters');
-				}
-			break;
-			case '>':
-				if (strlen(Input::get($var)) < $length) {
-					$this->setError($name.' must be greater than '.$length.' characters');
-				}
-			break;
-			case '<=':
-				if (strlen(Input::get($var)) >= $length) {
-					$this->setError($name.' must be maximum of '.$length.' characters');
-				}
-			break;
-			case '>=':
-				if (strlen(Input::get($var)) <= $length) {
-					$this->setError($name.' must be minimum of '.$length.' characters');
-				}
-			break;
-			default:
-					die('Oops! something went wrong, please check your conditions');
-		}
-	}
-
-	/**
-	 * to check if the follolwing case is true, default cases are check and remember for setting a setting
-	 *
-	 * @param string $action
-	 * @param string $name
-	 */
-	private function checkbox($action = 'check', $name) {
-		switch($action) {
-			case 'check':
-				echo (Input::get($name)) ? Input::get($name) : 'off';				
-			break;
-			case 'remember':
-				if (Input::get($name) == "on") {
-					$this->session_type = "cookies";
-				}
-			break;
-		}
-	}
-
-	/**
-	 * default exist system to check whether username/email exists, this can be extendable and is totally dependant
-	 *
-	 * @param string $var
-	 * @return void
-	 */
-	private function exists($var) {
-		$exists = $this->get('users', [
-										$var,
-										'=',
-										Input::get($var)
-									  ]);
-
-		$this->address = address();
-		
-		switch ($this->address) {
-			case 'signup':
-				if ($exists->_count == true) {
-					$this->setError(ucfirst($var).' already exists');
-				}
-			break;
-
-			case 'signin':
-				if ($exists->_count == false) {
-					$this->setError("No user exists! Have you registered ?");
-				}
-			break;
-		}
 	}
 }
